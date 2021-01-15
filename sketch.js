aaf45866e5c3c5cac13266091685546f9fe96929
adityakankariya <ditster1234@gmail.com>
2021-01-14 19:36:57 -0800

    Adjusting colors

--- sketch.js
+++ sketch.js
@@ -13,14 +13,16 @@ function draw() {
 	var m = map(minute(), 0, 60, 0, TWO_PI);
 	var h = map(hour(), 0, 24, 0, TWO_PI);
 
+	fill(232,247,255)
+	circle(width/2, height/2, 500); // Backdrop for seconds ring
 	fill(98, 168, 229);
 	arc(width/2, height/2, 500, 500, -HALF_PI, -HALF_PI + s); // Ring for seconds
-	fill(255);
-	circle(width/2, height/2, 425);
-	fill(0, 51, 102);
+	fill(0,76,154);
+	circle(width/2, height/2, 425); // Backdrop for minutes ring
+	fill(0, 38, 77);
 	arc(width/2, height/2, 425, 425, -HALF_PI, -HALF_PI + m); // Ring for minutes
-	fill(255);
-	circle(width/2, height/2, 350);
+	fill(225);
+	circle(width/2, height/2, 350); // Backdrop for hours ring
 	fill(150);
 	arc(width/2, height/2, 350, 350, -HALF_PI, -HALF_PI + h); // Ring for hours
 	
